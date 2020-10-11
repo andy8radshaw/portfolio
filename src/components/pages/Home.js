a90f373288bcac512e3571d27fad9c1a4e27b7ae
andy8radshaw <andy8radshaw@gmail.com>
2020-10-11 18:37:41 +0100

    more minor adjustments to nav

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,29 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react' 
+import { Link } from 'react-router-dom'
 import Header from '../common/Header'
 
 function Home() {
   return (
     <div className='Home'>
       <Header />
-      <h1>Andy Bradshaw</h1>
+      <p>A Junior Software Engineer embarking on my first role at <a href="https://www.reachplc.com/home" target="_blank">Reach Plc.</a> Previously graduating
+        from General Assembly’s Software Engineering Immersive course, where I obtained a strong
+        understanding of modern programming languages including HTML, CSS, JavaScript, React, Node.js & Python. My
+        journey into software engineering came from a love of problem solving and a fascination and curiosity for
+        all things tech.
+      </p>
+  
+      <p>Before all this coding took off, I worked in the audio industry for 10 years, designing and selling sound
+        systems into shops, offices, stadiums and churches. Projects I've worked on include; Harrods, Kings
+        Collage Chapel Cambridge, Body Worlds Ldn and many others. My time in the audio world has taught me a
+        great deal
+        about being adaptable, working as part of a team and delivering excellent results on time in an agile
+        environment. In my new role at Reach I will continue on the path of development, improving my
+        skills, and aim to become a valued and creative member of the team.</p>
+      <p>Please get in <Link to="/contact">contact</Link> if you are interested in working with me. Likewise, if you
+        would like some advice about your sound system or just want to chat guitars. Gibson ES-335 if you're
+        asking...</p>
     </div>
   )
 }
