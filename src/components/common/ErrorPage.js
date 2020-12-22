3e2f4d9f43c0e07e493eeafe1900a2d1edbb0561
andy8radshaw <andy8radshaw@gmail.com>
2020-12-22 11:37:15 +0000

    added dark mode to error page

--- src/components/common/ErrorPage.js
+++ src/components/common/ErrorPage.js
@@ -1,9 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 
-export default function ErrorPage() {
+export default function ErrorPage({ isLightMode }) {
   return (
-    <section className="hero is-white is-fullheight-with-navbar">
+    <section className={isLightMode ? 'hero is-light is-fullheight-with-navbar' : 'hero is-dark is-fullheight-with-navbar'}>
       <div className="hero-body">
         <div className="container">
           <p className="title">
